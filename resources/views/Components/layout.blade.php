5c46b926194767c5c9a9efb9b8fb36448f54a638
Rushikesh <rushi@rushi.local>
2024-10-08 13:41:47 +0530

    configures "css" and "js" by vite build

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Document</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite(['resources/js/app.js'])
 </head>
 <body class="h-full">
 <div class="min-h-full">
